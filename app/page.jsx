318214e5453a0e72167da56eded585a8babade2d
ArcBunkerDesigns <chinghosty1995@gmail.com>
2026-05-04 10:29:05 +0200

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{
+      background: "#05030a",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <h1>ARC BUNKER 🚀</h1>
+    </main>
+  );
+}
